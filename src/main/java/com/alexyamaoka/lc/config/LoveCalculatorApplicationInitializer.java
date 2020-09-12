e8723040026611c89832d522077cb6024f1b149f
alexYamaoka <ryamaoka1@gmail.com>
2020-09-11 22:32:39 -0700

    created class that extends web application initializer for configuration

---
+++ src/main/java/com/alexyamaoka/lc/config/LoveCalculatorApplicationInitializer.java
@@ -0,0 +1,16 @@
+package com.alexyamaoka.lc.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.springframework.web.WebApplicationInitializer;
+
+public class LoveCalculatorApplicationInitializer implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
