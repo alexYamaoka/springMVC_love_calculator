19751b08239e70bea9f2ecd5ec6f785af07fee70
alexYamaoka <ryamaoka1@gmail.com>
2020-09-12 00:08:54 -0700

    added annotation enablewebmvc

--- src/main/java/com/alexyamaoka/lc/config/LoveCalculatorAppConfig.java
+++ src/main/java/com/alexyamaoka/lc/config/LoveCalculatorAppConfig.java
@@ -4,9 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceView;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+@EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "com.alexyamaoka.lc.controllers")
 public class LoveCalculatorAppConfig {
