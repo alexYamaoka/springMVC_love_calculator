bcbbf2c5aca1e2af7262efa696db2cfa7304f8a1
alexYamaoka <ryamaoka1@gmail.com>
2020-09-11 22:56:27 -0700

    connected dispatcher servlet to servletContext

--- src/main/java/com/alexyamaoka/lc/config/LoveCalculatorApplicationInitializer.java
+++ src/main/java/com/alexyamaoka/lc/config/LoveCalculatorApplicationInitializer.java
@@ -4,6 +4,8 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 public class LoveCalculatorApplicationInitializer implements WebApplicationInitializer {
@@ -11,8 +13,13 @@ public class LoveCalculatorApplicationInitializer implements WebApplicationIniti
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		
+		XmlWebApplicationContext webApplicationContext = new XmlWebApplicationContext();
+		webApplicationContext.setConfigLocation("classpath:application-config.xml");
+		
 		// create a dispatcher servlet object 
-		DispatcherServlet dispatcherServlet = new DispatcherServlet();
+		DispatcherServlet dispatcherServlet = new DispatcherServlet(webApplicationContext);
+		
+		servletContext.addServlet("myDispatcherServlet", dispatcherServlet)
 	}
 
 }
