9c122230036527e17159c5574a24a4fcf32b69dd
alexYamaoka <ryamaoka1@gmail.com>
2020-09-15 20:17:47 -0700

    added Model object to send data to result page

--- src/main/java/com/alexyamaoka/lc/controllers/LCAppController.java
+++ src/main/java/com/alexyamaoka/lc/controllers/LCAppController.java
@@ -1,6 +1,7 @@
 package com.alexyamaoka.lc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -9,14 +10,21 @@ public class LCAppController {
 	
 	@RequestMapping("/")
 	public String showHomePage() {
+		
 		return "home-page";
 	}
 	
+	
 	@RequestMapping("/process-homepage")
-	public String showResultPage(@RequestParam String username, @RequestParam String crushName) {
+	public String showResultPage(Model model, @RequestParam String username, @RequestParam String crushName) {
 		// @RequestParam - used for data binding form parameter to variables
+		// to change variable name: @RequestParam("username") String UN 
+		
 		System.out.println("username: " + username);
 		System.out.println("crush name: " + crushName);
+		
+		model.addAttribute("username", username);
+		model.addAttribute("crushName", crushName);
 		
 		return "result-page";
 	}
