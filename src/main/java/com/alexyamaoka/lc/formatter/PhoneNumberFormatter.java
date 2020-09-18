f8aef94ba99848bc6cd16a0686ebb26bda2582de
alexYamaoka <ryamaoka1@gmail.com>
2020-09-17 21:04:02 -0700

    added phone number formatting logic

--- src/main/java/com/alexyamaoka/lc/formatter/PhoneNumberFormatter.java
+++ src/main/java/com/alexyamaoka/lc/formatter/PhoneNumberFormatter.java
@@ -19,8 +19,16 @@ public class PhoneNumberFormatter implements Formatter<Phone>{
 	public Phone parse(String text, Locale locale) throws ParseException {
 		System.out.println("inside parse method of Phone Number Formatter");
 		
-		// logic
-		return null;
+		// logic to separate country code with number  1-310xxxxxxx 
+		String[] phoneNumberArray = text.split("-");
+		Phone phone = new Phone();
+		phone.setCountryCode(phoneNumberArray[0]);
+		phone.setUserNumber(phoneNumberArray[1]);
+		
+		System.out.println("country code: " + phone.getCountryCode());
+		System.out.println("phone number: " + phone.getUserNumber());
+		
+		return phone;
 	}
 
 }
