ca8f1c4d8050aafea97d36cf02252c1bc44decb1
alexYamaoka <ryamaoka1@gmail.com>
2020-09-21 21:22:01 -0700

    wrote logic for isValid for age validator

--- src/main/java/com/alexyamaoka/lc/validator/AgeValidator.java
+++ src/main/java/com/alexyamaoka/lc/validator/AgeValidator.java
@@ -1,5 +1,36 @@
 package com.alexyamaoka.lc.validator;
 
-public class AgeValidator {
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+// dont need annotation like component because of ConstraintValidator interface
+public class AgeValidator implements ConstraintValidator<Age, Integer> {
+
+	private int lower;
+	private int upper;
+
+	@Override
+	public void initialize(Age age) {
+
+		// for post construct work
+		this.lower = age.lower();
+		this.upper = age.upper();
+	}
+
+	@Override
+	public boolean isValid(Integer age, ConstraintValidatorContext context) {
+
+		// need to write validation logic here
+
+		if (age == null) {
+			return false;
+		}
+
+		if (age < this.lower || age > this.upper) {
+			return false;
+		}
+
+		return true;
+	}
 
 }
