5dee4bb35ac0a9f74224e044af199ebcda42d03b
alexYamaoka <ryamaoka1@gmail.com>
2020-09-23 10:21:02 -0700

    created converter for Credit Card object

---
+++ src/main/java/com/alexyamaoka/lc/converters/CreditCardConverter.java
@@ -0,0 +1,27 @@
+package com.alexyamaoka.lc.converters;
+
+import org.springframework.core.convert.converter.Converter;
+
+import com.alexyamaoka.lc.api.CreditCard;
+
+public class CreditCardConverter implements Converter<String, CreditCard>{
+
+	@Override
+	public CreditCard convert(String source) {
+		
+		
+		String[] cardNumberArray = source.split("-");
+
+		CreditCard creditCard = new CreditCard();
+
+		creditCard.setFirstFourDigits(Integer.parseInt(cardNumberArray[0]));
+		creditCard.setSecondFourDigits(Integer.parseInt(cardNumberArray[1]));
+		creditCard.setThirdFourDigits(Integer.parseInt(cardNumberArray[2]));
+		creditCard.setFourthFourDigits(Integer.parseInt(cardNumberArray[3]));
+		
+		
+		
+		return creditCard;
+	}
+
+}
