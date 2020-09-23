9b11093a45f146c08e066034e545dd15a88fe40b
alexYamaoka <ryamaoka1@gmail.com>
2020-09-22 20:50:39 -0700

    added property editor for date inside payment controller

--- src/main/java/com/alexyamaoka/lc/controllers/PaymentController.java
+++ src/main/java/com/alexyamaoka/lc/controllers/PaymentController.java
@@ -1,5 +1,9 @@
 package com.alexyamaoka.lc.controllers;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
@@ -7,7 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.alexyamaoka.lc.api.BillDTO;
-import com.alexyamaoka.lc.api.Billing;
+
 
 
 @Controller
@@ -28,5 +32,12 @@ public class PaymentController {
 	@InitBinder
 	public void initBinder(WebDataBinder webDataBinder) {
 		System.out.println("inside init binder of payment controller");
+		
+		// property editor
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("mm-dd-yyyy");
+		CustomDateEditor customDateEditor = new CustomDateEditor(simpleDateFormat, true);
+		webDataBinder.registerCustomEditor(Date.class, "date", customDateEditor);
+		
+		
 	}
 }
