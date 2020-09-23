c45e4cb8f3eae5c8815fd819d0c63b5cc98382a4
alexYamaoka <ryamaoka1@gmail.com>
2020-09-23 14:22:24 -0700

    added modelAttribute to email controller

---
+++ src/main/java/com/alexyamaoka/lc/api/EmailDTO.java
@@ -0,0 +1,15 @@
+package com.alexyamaoka.lc.api;
+
+public class EmailDTO {
+	
+	private String userEmail;
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	
+}
--- src/main/java/com/alexyamaoka/lc/controllers/EmailController.java
+++ src/main/java/com/alexyamaoka/lc/controllers/EmailController.java
@@ -1,13 +1,16 @@
 package com.alexyamaoka.lc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.alexyamaoka.lc.api.EmailDTO;
 
 @Controller
 public class EmailController {
 	
 	@RequestMapping("/sendEmail")
-	public String sendEmail() {
+	public String sendEmail(@ModelAttribute("emailDTO") EmailDTO emailDTO) {
 		
 		return "send-email-page";
 	}
