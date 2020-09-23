889a2ea461f67959dedac1543f1414aaaa12e1ed
alexYamaoka <ryamaoka1@gmail.com>
2020-09-22 21:17:41 -0700

    created custom property editor to make name field uppercase

--- src/main/java/com/alexyamaoka/lc/propertyEditor/NamePropertyEditor.java
+++ src/main/java/com/alexyamaoka/lc/propertyEditor/NamePropertyEditor.java
@@ -3,8 +3,12 @@ package com.alexyamaoka.lc.propertyEditor;
 import java.beans.PropertyEditorSupport;
 
 public class NamePropertyEditor extends PropertyEditorSupport {
-	
-	
-	
 
+	@Override
+	public void setAsText(String text) throws IllegalArgumentException {
+		
+		String myConvertedStringValue = text.toUpperCase();
+		
+		setValue(myConvertedStringValue);
+	}
 }
