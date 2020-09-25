6cf19568b30ca290491a4c08dbb676da20480df8
alexYamaoka <ryamaoka1@gmail.com>
2020-09-25 12:28:54 -0700

    added dummy values for java mail sender configuration Bean method

--- src/main/java/com/alexyamaoka/lc/config/LoveCalculatorAppConfig.java
+++ src/main/java/com/alexyamaoka/lc/config/LoveCalculatorAppConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.ViewResolver;
@@ -80,4 +82,32 @@ public class LoveCalculatorAppConfig implements WebMvcConfigurer {
 		
 		return validator();
 	}
+	
+	
+	
+	
+	
+	// configure java mail sender 
+	@Bean 
+	public JavaMailSender configureJavaMailSender() {
+		
+		JavaMailSenderImpl javaMailSenderImpl = new JavaMailSenderImpl();
+		
+		javaMailSenderImpl.setHost("smtp.gmail.com");
+		javaMailSenderImpl.setUsername("sampleEmail@gmail.com");
+		javaMailSenderImpl.setPassword("samplePassword");
+		javaMailSenderImpl.setPort(567);
+		
+		
+		
+		return javaMailSenderImpl;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
